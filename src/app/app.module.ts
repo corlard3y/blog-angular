c0cae3842116918d0de16cecec483c81fa6658f1
Kolade <corlardey@gmail.com>
2021-05-18 23:39:06 +0100

    nav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,20 +4,21 @@ import { HttpClientModule } from '@angular/common/http';
 import { HttpClientInMemoryWebApiModule } from "angular-in-memory-web-api";
 import { InMemoryDataService } from './in-memory-data.service';
 import { FormsModule } from '@angular/forms';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PostComponent } from './components/post/post.component';
 import { PostsComponent } from './components/posts/posts.component';
 import { PostsService } from './components/posts/posts.service';
 import { CreatePostComponent } from './components/create-post/create-post.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PostComponent,
     PostsComponent,
-    CreatePostComponent
+    CreatePostComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
